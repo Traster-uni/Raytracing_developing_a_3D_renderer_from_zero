04c09f5f1b233a5ba39af9242e2b74db82dd28b3
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2024-07-07 18:55:15 +0200

    Completed Cmake configuration

---
+++ libs/r3df0_math.h
@@ -0,0 +1,27 @@
+//
+// Created by trast on 01/07/2024.
+//
+
+#ifndef _R3DFROM0_MATH_H
+#define _R3DFROM0_MATH_H
+
+// INCLUDES
+#include <cmath>
+
+namespace r3dfrom0 {
+    // floats
+    struct vec3f {
+        float x = 0;
+        float y = 0;
+        float z = 0;
+    };
+
+    //integers
+    struct vec3i {
+        int x = 0;
+        int y = 0;
+        int z = 0;
+    };
+} // namespace r3dfrom0
+
+#endif //_R3DFROM0_MATH_H
---
+++ src/r3df0_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "r3df0_math.h"
+using namespace std;
+using namespace r3dfrom0;
+
+int main(){
+    vec3f p = {1,2,3};
+    cout << "HELLO WORLD! " << p.x << "," << p.y << "," << p.z << endl;
+}
